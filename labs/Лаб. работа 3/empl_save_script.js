5ea764be33b4a536785bcf66a07b98d828d93735
pavelyolvev <31282106+pavelyolvev@users.noreply.github.com>
2023-10-31 13:27:36 +0400

    Delete labs directory

--- labs/Лаб. работа 3/empl_save_script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var name = new URLSearchParams(window.location.search).get("name");
-    var txtEmplName = document.getElementById("txt_empl_name");
-
-    txtEmplName.value = name;
-});
